a7bb410e830b1f1f93ec4a444ee1963299cc4e97
Anjeela <anjeelamhrzn04@gmail.com>
2026-06-02 12:05:35 +0545

    graphql apollo provider added

--- MyDrawerApp/app/_layout.tsx
+++ MyDrawerApp/app/_layout.tsx
@@ -1,23 +1,31 @@
 import { Stack } from "expo-router";
+import { ApolloClient, InMemoryCache,HttpLink} from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from '@/context/ThemeContext';
 import { DrawerProvider } from '@/features/navigation/context/DrawerContext';
 import { CounterProvider } from '@/features/counter/context/CounterContext';
 
-/**
- * Root Layout for Expo Router.
- * Configures the primary stack and mounts global state providers at the top level.
- */
+export const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://rickandmortyapi.com/graphql',}),
+    // uri: 'https://graphqlzero.almansi.me/api',}),
+  cache: new InMemoryCache(),
+});
+
 export default function RootLayout() {
+
   return (
     <SafeAreaProvider>
       <ThemeProvider>
         <DrawerProvider>
           <CounterProvider>
-            <Stack screenOptions={{ headerShown: false }}>
-              <Stack.Screen name="(drawer)" />
-              <Stack.Screen name="pokemon/[id]" />
-            </Stack>
+            <ApolloProvider client={client}>
+              <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="(drawer)" />
+                <Stack.Screen name="pokemon/[id]" />
+              </Stack>
+            </ApolloProvider>
           </CounterProvider>
         </DrawerProvider>
       </ThemeProvider>
