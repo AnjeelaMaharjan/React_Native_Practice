06212526e911e465fc1b8bd6f48a3b709cb24beb
Anjeela <anjeelamhrzn04@gmail.com>
2026-06-18 16:10:56 +0545

    MyDrawerApp/drizzle.config.ts modified

--- MyDrawerApp/drizzle.config.ts
+++ MyDrawerApp/drizzle.config.ts
@@ -2,7 +2,9 @@ import {defineConfig} from 'drizzle-kit';
 
 export default defineConfig({
     dialect: 'sqlite',
-    driver: 'expo',
     schema: './src/db/schema.ts',
     out: './drizzle',
+    dbCredentials: {
+        url: './offline_first_app.db'
+    }
 })
